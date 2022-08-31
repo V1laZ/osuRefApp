dc6b958b136a22fd4a5c74b6c59ed544896ef656
V1laZ <pasikstudiocz@gmail.com>
2022-08-31 14:25:39 +0200

    remove unused funcs

--- app/MainApp.py
+++ app/MainApp.py
@@ -25,65 +25,6 @@ class MyMainApp(MDApp):
         kv = Builder.load_file("style.kv")
         return kv
 
-    # def add_mappoolTableGS(self, link, listName):
-    #     """Initialize table widget with extracted mappool from Google Sheets and add it to mappoolAdd screen"""
-    #     mappool = extractMapPools(link, listName)
-
-    #     self.mappoolTableGS = MDDataTable(
-    #         rows_num = len(mappool[0]),
-    #         size_hint = (0.7, 0.7),
-    #         pos_hint= {"center_x": 0.5, "top": 0.95},
-    #         column_data=[
-    #             ("Type", dp(15)),
-    #             ("Map ID", dp(15)),
-    #         ],
-    #         row_data=[
-                
-    #         ]
-    #     )
-
-    #     for i in mappool[0]:
-    #         print(i)
-    #         self.mappoolTableGS.row_data.append(i)
-
-    #     self.mappoolTableGS.bind(on_row_press=self.mappoolTableGS_row_pressed)
-    #     self.root.ids.mappool_add.ids.floatlayout.add_widget(self.mappoolTableGS)
-
-    def mappoolTableGS_row_pressed(self, instance_table, instance_row):
-        popup = Popup(
-            title = "Change value",
-            size_hint = (0.8, 0.3)
-        )
-
-        layout = FloatLayout()
-
-        textinput = TextInput(
-            text = instance_row.text,
-            pos_hint = {"top": 0.85, "center_x": 0.5},
-            size_hint = (0.8, 0.27),
-            halign = "center"
-        )
-
-        btn = Button(
-            text = "Save",
-            pos_hint = {"center_x": 0.5, "top": 0.38},
-            size_hint =  (0.26, 0.27),
-            on_release = lambda x: self.mappoolTableGS_save_pressed(textinput.text, popup, instance_row, instance_table)
-        )
-
-        layout.add_widget(textinput)
-        layout.add_widget(btn)
-        popup.add_widget(layout)
-        popup.open()
-
-    def mappoolTableGS_save_pressed(self, text, popup_instance, instance_row, instance_table):
-        for map in instance_table.row_data:
-            for value in map:
-                if value == instance_row.text:
-                    self.mappoolTableGS.row_data[instance_table.row_data.index(map)][map.index(value)] = text
-        self.mappoolTableGS.update_row_data(self.mappoolTableGS, self.mappoolTableGS.row_data)
-        popup_instance.dismiss()
-
     def addMappoolTable(self):
         """Initialize mappool table widget and add it to mappoolInit screen"""
         if os.path.exists("mappools/") is False:
@@ -96,8 +37,7 @@ class MyMainApp(MDApp):
 
         self.mappool_table = MDDataTable(
             rows_num = 10,
-            size_hint = (0.7, 0.53),
-            pos_hint = {"center_x": 0.5, "top": 0.63},
+            pos_hint = {"center_x": 0.5},
             column_data = [
                 ("Name", dp(30)),
                 ("", dp(1))
@@ -111,7 +51,7 @@ class MyMainApp(MDApp):
             self.mappool_table.row_data.append((name, ""))
         
         self.mappool_table.bind(on_row_press=self.mappool_row_pressed)
-        self.root.ids.mappool_init.ids.floatlayout.add_widget(self.mappool_table)
+        self.root.ids.mappool_init.ids.boxlayout.add_widget(self.mappool_table, index=3)
 
     def mappool_row_pressed(self, instance_table, instance_row):
         """Called when row is pressed in mappool_table widget. Initialize and show new window with corresponding mappool"""
